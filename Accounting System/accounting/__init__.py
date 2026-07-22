75c9b1fa48c2bb54bdf4776e87d767cb0e9a71c8
鈴木麗央 <137374045+goheimotiiiii@users.noreply.github.com>
2026-07-22 20:34:13 +0900

    Delete Accounting System directory

--- Accounting System/accounting/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Accounting system package."""
-
-from .models import Ledger, Transaction, TransactionItem
-from .storage import load_ledger, save_ledger
-
-__all__ = ["Ledger", "Transaction", "TransactionItem", "load_ledger", "save_ledger"]
